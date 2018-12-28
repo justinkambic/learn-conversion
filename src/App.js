ca4f5d47c0c97d40ceda2cd6437fd81b58ea3362
Saro123 <Saro898@gmail.com>
2018-12-27 21:04:28 -0500

    Ran Prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-     <div>
-
-       <h1 className="Hello-World-header">Hello World</h1>
-
-       </div>
-
+      <div>
+        <h1 className="Hello-World-header">Hello World</h1>
+      </div>
     );
   }
 }
